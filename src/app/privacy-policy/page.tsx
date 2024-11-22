c12139de2d7b898efb6edaf4b71ef9edcd799d88
sumit <sk7627577@gmail.com>
2024-11-22 12:48:00 +0530

    update

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useState } from "react";
+import React from "react";
 import { Metadata } from "next";
 
 
--- src/app/refund-policy/page.tsx
+++ src/app/refund-policy/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Metadata } from "next";
 
 
